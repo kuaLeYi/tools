761e7d7383aa7d1f6ee4675292d02b9947ba1194
kuaLeYi <23028142+kuaLeYi@users.noreply.github.com>
2020-01-05 14:53:01 +0800

    Fixes link and simplifies script names

--- text-assistant/dlmcolxtracscrp.js
+++
@@ -1,5 +0,0 @@
-function extractDlmCol() {
-  navigateBttm(bttmdlmcolxtrac);
-  var n = dlmcolxtracnum.value, d = colxtracdlmr.value;
-  dlmcolxtracres.value = outplines().map(e => n > 0 && n%1 === 0 ? e.split(d)[n-1] : '').join`\n`;
-}
--- text-assistant/presufaddrmvscrp.js
+++
@@ -1,9 +0,0 @@
-function preSufAdd() {
-  outp.value = outplines().map(e => presufa.value + e + presufb.value).join`\n`;
-  fcs();
-}
-
-function preSufRmv() {
-  outp.value = outplines().map(e => e.replace(new RegExp('^'+presufc.value), '').replace(new RegExp(presufd.value+'$'), '')).join`\n`;
-  fcs();
-}
