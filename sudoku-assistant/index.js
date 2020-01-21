9de9ba29590c8bd3709e256bf0210d36874d34cc
kuaLeYi <23028142+kuaLeYi@users.noreply.github.com>
2020-01-21 17:51:08 +0800

    Updates index.html and sudoku-assistant/index.js

--- sudoku-assistant/index.js
+++ sudoku-assistant/index.js
@@ -1,15 +1,16 @@
 var board,
   substit = '123456789'.split``.map(e => [e, Math.random()]).sort((a, b) => a[1]-b[1]).map(e => e[0]),
   rotat = arr => arr.map((e, i, a) => a.map(e => e[e.length-i-1]).join``),
-  transform = arr => {
+  transform = x => {
     var rotMode = Math.random(), mirrMode = Math.random();
     if (rotMode < .75) arr = rotat(arr);
     if (rotMode < .5) arr = rotat(arr);
     if (rotMode < .25) arr = rotat(arr);
     if (mirrMode < .5) arr = arr.map(e => e.split``.reverse().join``);
+    return arr;
   },
   l = '000700000\n100000000\n000430200\n000000006\n000509000\n000000418\n000081000\n002000050\n040000300'.replace(/[1-9]/g, e => substit.indexOf(e)+1).split`\n`;
-transform(l);
+l = transform(l);
 function example(){
   main(inp.value = l.join`\n`);
   b.click();
